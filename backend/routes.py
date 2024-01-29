aad2a160d7cd879498ef268578abb0bcb7ba3ffe
ORMDV <moncadarodrigo7710@gmail.com>
2024-01-28 18:28:06 -0600

    Add files via upload

---
+++ backend/routes.py
@@ -0,0 +1,103 @@
+from . import app
+import os
+import json
+from flask import jsonify, request, make_response, abort, url_for  # noqa; F401
+
+SITE_ROOT = os.path.realpath(os.path.dirname(__file__))
+json_url = os.path.join(SITE_ROOT, "data", "pictures.json")
+data: list = json.load(open(json_url))
+
+######################################################################
+# RETURN HEALTH OF THE APP
+######################################################################
+
+
+@app.route("/health")
+def health():
+    return jsonify(dict(status="OK")), 200
+
+######################################################################
+# COUNT THE NUMBER OF PICTURES
+######################################################################
+
+
+@app.route("/count")
+def count():
+    """return length of data"""
+    if data:
+        return jsonify(length=len(data)), 200
+
+    return {"message": "Internal server error"}, 500
+
+
+######################################################################
+# GET ALL PICTURES
+######################################################################
+@app.route("/picture", methods=["GET"])
+def get_pictures():
+	return data
+#	return {"message": f"data"}, 200
+   # pass
+
+######################################################################
+# GET A PICTURE
+######################################################################
+
+
+@app.route("/picture/<int:id>", methods=["GET"])
+def get_picture_by_id(id):
+#	picture = data
+#	picURL = picture[id]
+#	return {"URL": picURL["pic_url"]}, 200
+   # pass
+	for picture in data:
+		if picture["id"] == id:
+			return picture
+	return {"message": "picture not found"}, 404
+
+######################################################################
+# CREATE A PICTURE
+######################################################################
+@app.route("/picture", methods=["POST"])
+def create_picture():
+#    pass
+	picture_in = request.json
+	print(picture_in)
+
+	for picture in data:
+		if picture_in["id"] == picture["id"]:
+			return {
+				"message": f"picture with id {picture_in['id']} already present"
+			}, 302
+
+	data.append(picture_in)
+	return picture_in, 201
+######################################################################
+# UPDATE A PICTURE
+######################################################################
+
+
+@app.route("/picture/<int:id>", methods=["PUT"])
+def update_picture(id):
+#    pass
+	picture_in = request.json
+
+	for index, picture in enumerate(data):
+		if picture["id"]  == id:
+			data[index] = picture_in
+			return picture, 201
+	return {
+		"message": f"picture not found"
+	}, 404
+
+######################################################################
+# DELETE A PICTURE
+######################################################################
+@app.route("/picture/<int:id>", methods=["DELETE"])
+def delete_picture(id):
+#    pass
+	for picture in data:
+		if picture["id"] == id:
+			data.remove(picture)
+			return "", 204
+	return {"message": "picture not found"}, 404
